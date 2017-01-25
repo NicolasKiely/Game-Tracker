56024f49861772e349d9efbdf240ef19d736ad51
nmkiely <nmkiely@ncsu.edu>
2017-01-25 15:01:28 -0500

    List leagues in top-level manager page
    @UC Dashboard
    @UC League Manager
    Top level manager for leagues is operational

--- urls.py
+++ urls.py
@@ -4,6 +4,13 @@ from . import views
 urlpatterns = [
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^view/$', views.public_view, name='view'),
+
+    # League pages
     url(r'^league/editor/$', views.editor_league_list, name='league_manager'),
+    url(r'^league/editor/(?P<pk>\d+)/\w*$', views.league_editor, name='league_editor'),
+    url(r'^league/view/(?P<pk>\d+)/\w*$', views.league_view, name='league_view'),
+    url(r'^league/add/$', views.add_league, name='add_league'),
+
+    # Team pages
     url(r'^team/editor/$', views.editor_team_list, name='team_manager')
 ]
--- views.py
+++ views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from common import core
 from .models import League
 
 
+@login_required
 def dashboard(request):
     ''' Admin dashboard for gametracker '''
     context = {
@@ -15,28 +19,45 @@ def public_view(request):
     ''' Public page for gametracker '''
     pass
 
+
+@login_required
 def editor_league_list(request):
     ''' Top-level editor for leagues '''
     context = {
+        'title'  : 'League Manager',
+        'leagues': League.objects.all()
     }
     return core.render(request, 'gametracker/leagueManager.html', **context)
 
 
-def editor_league(request):
+@login_required
+def add_league(request):
+    ''' Post for adding league '''
+    lgName = request.POST['name']
+    lgHiWins = True if 'hiwins' in request.POST else False
+
+    league = League(name=lgName, highWins=lgHiWins)
+    league.save()
+
+    return HttpResponseRedirect(
+        reverse('gametracker:league_manager')
+    )
+
+
+@login_required
+def league_editor(request, pk):
     pass
 
 
-def view_league(request):
+def league_view(request, pk):
     pass
 
 
+@login_required
 def edit_league(request):
     pass
 
 
-def add_league(request):
-    pass
-
-
+@login_required
 def editor_team_list(request):
     pass
